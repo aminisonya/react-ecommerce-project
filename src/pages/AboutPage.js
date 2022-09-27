cbafc6b080b5e7a731a50f348cfe0c9c33006662
Sonya Amini <soamini@microsoft.com>
2022-09-26 23:26:39 -0700

    more steady UI elements

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -4,7 +4,21 @@ import { PageHero } from '../components'
 import aboutImg from '../assets/hero-bcg.jpeg'
 
 const AboutPage = () => {
-  return <h4>about page</h4>
+  return <main>
+    <PageHero title='about' />
+    <Wrapper className='page section section-center'>
+      <img src={aboutImg} alt='nice desk' />
+      <article>
+        <div className='title'>
+          <h2>our story</h2>
+          <div className='underline'></div>
+        </div>
+        <p>
+          This is the story of how this brand/company/etc came into existence. Who runs this. Why did they start this. How did they come together. How long have they been together, and doing this.
+        </p>
+      </article>
+    </Wrapper>
+  </main>
 }
 
 const Wrapper = styled.section`

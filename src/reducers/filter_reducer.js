4568c3f4e34e2cbd1baf3d6c14d556d90fe88fa1
Sonya Amini <soamini@microsoft.com>
2022-10-10 18:20:56 -0700

    added filter for text

--- src/reducers/filter_reducer.js
+++ src/reducers/filter_reducer.js
@@ -39,43 +39,50 @@ const filter_reducer = (state, action) => {
   }
 
   if (action.type === SORT_PRODUCTS) {
-    const {sort, filtered_products} = state;
+    const { sort, filtered_products } = state;
     let tempProducts = [...filtered_products];
 
     if (sort === 'price-lowest') {
-      tempProducts = tempProducts.sort((a,b) => a.price - b.price)
+      tempProducts = tempProducts.sort((a, b) => a.price - b.price)
     }
     if (sort === 'price-highest') {
-      tempProducts = tempProducts.sort((a,b) => b.price - a.price)
+      tempProducts = tempProducts.sort((a, b) => b.price - a.price)
     }
     if (sort === 'name-a') {
-      tempProducts = tempProducts.sort((a,b) => {
+      tempProducts = tempProducts.sort((a, b) => {
         return a.name.localeCompare(b.name);
       })
     }
     if (sort === 'name-z') {
-      tempProducts = tempProducts.sort((a,b) => {
+      tempProducts = tempProducts.sort((a, b) => {
         return b.name.localeCompare(a.name);
       })
     }
-    return {...state, filtered_products: tempProducts}
+    return { ...state, filtered_products: tempProducts }
   }
 
   if (action.type === UPDATE_FILTERS) {
-    const {name, value} = action.payload;
+    const { name, value } = action.payload;
     return { ...state, filters: { ...state.filters, [name]: value } };
   }
 
   if (action.type === FILTER_PRODUCTS) {
-    const {all_products} = state;
-    const {text, category, company, color, price, shipping} = state.filters;
+    const { all_products } = state;
+    const { text, category, company, color, price, shipping } = state.filters;
     let tempProducts = [...all_products];
+
+    //filtering
+    if (text) {
+      tempProducts = tempProducts.filter((product) => {
+        return product.name.toLowerCase().startsWith(text);
+      })
+    }
 
     return { ...state, filtered_products: tempProducts };
   }
 
   if (action.type === CLEAR_FILTERS) {
-    return { 
+    return {
       ...state,
       filters: {
         ...state.filters,
